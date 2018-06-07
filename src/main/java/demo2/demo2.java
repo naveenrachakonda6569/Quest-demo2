6a6619db15e99dba6093977cd36fd160a369d2f8
naveenrachakonda6569 <navin6569r@gmail.com>
2018-06-07 16:12:57 +0530

    intial commit

---
+++ src/main/java/demo2/demo2.java
@@ -0,0 +1,8 @@
+package demo2;
+
+public class demo2 {
+	public String sample() {
+		return "sample program";
+	}
+
+}
---
+++ src/test/java/demo2/questApp2.java
@@ -0,0 +1,14 @@
+package demo2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class questApp2 {
+
+	@Test
+	public void test() {
+	
+	}
+
+}
